381c9fb5bad76cb0d13530f955577c8712eb427d
bestcoderg <624303639@qq.com>
2018-12-31 00:32:51 +0800

    添加了鼠标图层，原谅我还不是很懂。。

--- kernel/main.c
+++ kernel/main.c
@@ -3,7 +3,7 @@
 #include <video.h>
 #include <keymap.h>
 #include "mouse.h"
-
+#include "sheet.h"
 int x,y; //鼠标的坐标
 int mx,my;
 void main(void)
@@ -13,23 +13,43 @@ void main(void)
 	char mcursor[256];
 	MOUSE_DEC mdec;
 	char s[40];
+	SHTCTL *shtctl;
+	SHEET *sht_back, *sht_mouse;
+	unsigned char *buf_back;
 
-
-	clear_screen();
+	//clear_screen();
 	init_gdt();
 	init_idt();
+	io_sti();
 
-	init_palette();/* 设定调色板 */
 	init_keyboard();
 	init_mouse();
-	
-	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);/* 加载背景色 */
+	init_palette();/* 设定调色板 */
+	mdec.phase = 0;
 
+
+	shtctl = shtctl_init(binfo->vram, binfo->scrnx, binfo->scrny);
+	sht_back  = sheet_alloc(shtctl);
+	sht_mouse = sheet_alloc(shtctl);
+	buf_back = (unsigned char *) 0x810000;
+	
+	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1); /* 没有透明色 */
+	sheet_setbuf(sht_mouse, mcursor, 16, 16, 99); /* 透明色号99 */
+	 
+
+	init_screen8(buf_back, binfo->scrnx, binfo->scrny);/* 加载背景色 */
+	//init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);/* 加载背景色 */
 	init_mouse_cursor(mcursor, COL8_848484);
 	mx = (binfo->scrnx - 16) / 2;
     my = (binfo->scrny - 28 - 16) / 2;  
-	putblock(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* 描画鼠标 */
-				
+
+	//putblock(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* 描画鼠标 */
+	
+
+    sheet_slide(shtctl, sht_mouse, mx, my);
+	sheet_updown(shtctl, sht_back,  0);
+	sheet_updown(shtctl, sht_mouse, 1);
+
 	//x,y代表当前输出字符的位置
 	x = 8;
 	y = 24;
@@ -61,7 +81,7 @@ void main(void)
 					s[2] = 'C';
 				}
 				/* 鼠标指针的移动 */
-				boxfill8(binfo->vram, binfo->scrnx, COL8_848484, mx, my, mx + 15, my + 15); /* 隐藏鼠标 */
+				//boxfill8(binfo->vram, binfo->scrnx, COL8_848484, mx, my, mx + 15, my + 15); /* 隐藏鼠标 */
 				mx += mdec.x;
 				my += mdec.y;
 				if (mx < 0) {
@@ -76,8 +96,8 @@ void main(void)
 				if (my > binfo->scrny - 16) {
 					my = binfo->scrny - 16;
 				}
-				putblock(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* 描画鼠标 */
-				//sheet_slide(shtctl, sht_mouse, mx, my); /* 包含sheet_refresh含sheet_refresh */
+				//putblock(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* 描画鼠标 */
+				sheet_slide(shtctl, sht_mouse, mx, my); /* 包含sheet_refresh含sheet_refresh */
 			}
 		}
 	}
